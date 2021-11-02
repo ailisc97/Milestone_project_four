0c71be80b879e77d7e6b160eb35f452ac4708bbc
ailisc97 <ailiscurran97@gmail.com>
2021-11-02 19:02:20 +0000

    added url.py and views.py to the home dir

--- Milestone_project_four/settings.py
+++ Milestone_project_four/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,11 @@ ROOT_URLCONF = 'Milestone_project_four.urls'
 
 TEMPLATES = [{
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
