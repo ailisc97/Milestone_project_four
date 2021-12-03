fde3cc2d4d7136a667a2b7142e5a7098102feb54
ailisc97 <ailiscurran97@gmail.com>
2021-12-03 13:30:52 +0000

    blog and comments done

--- Milestone_project_four/settings.py
+++ Milestone_project_four/settings.py
@@ -72,10 +72,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-            os.path.join(BASE_DIR, 'templates', 'blog'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.views import generic
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import CommentForm
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/blog_detail.html', {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
+   # return render(request, 'blog/blog_detail.html', {'post': post})
