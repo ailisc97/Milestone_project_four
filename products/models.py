227913b463ade9d4f22ecf17eed7ac6f24bf9169
ailisc97 <ailiscurran97@gmail.com>
2021-12-06 23:02:34 +0000

    cloudify

--- products/models.py
+++ products/models.py
@@ -29,6 +29,6 @@ class Product(models.Model):
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     # image = models.ImageField(null=True, blank=True)
-    image = models.CloudinaryField('image', null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)
     def __str__(self):
         return self.name
