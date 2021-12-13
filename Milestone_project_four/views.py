182aaad1b13d5120fc6a114b5548ec64a018a8bf
ailisc97 <ailiscurran97@gmail.com>
2021-12-13 13:08:00 +0000

    read me

--- Milestone_project_four/views.py
+++ Milestone_project_four/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
-def handler404(request, *args, **argv):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
+def handler404(request, exception, template_name="404.html"):
+    response = render_to_response(template_name)
     response.status_code = 404
     return response
 
