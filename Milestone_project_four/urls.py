6a6d7b2804178f8ea1a3236107f27556c8f498f8
ailisc97 <ailiscurran97@gmail.com>
2021-12-13 13:05:21 +0000

    read me

--- Milestone_project_four/urls.py
+++ Milestone_project_four/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler404 = 'my_app.views.handler404'
+handler500 = 'my_app.views.handler500'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
